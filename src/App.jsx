717a1fc121e83392fd8d32a4592c7fc3874d571d
Young <youngheesomemoto@gmail.com>
2024-02-12 21:32:34 -0600

    still strying to follow.... can't wait till im a wizard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,103 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { getPlayers, getPlayer } from "./api";
+import { PlayerDetails } from "./components/PlayerDetails";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [player, setPlayer] = useState({});
+
+  useEffect(() => {
+    getPlayers().then((players) => {
+      setPlayers(players);
+    });
+  }, []);
+
+  function handlePlayerClick(playerId) {
+    getPlayer(playerId).then(setPlayer);
+  }
 
   return (
     <>
-     <h1>Puppy Bowl</h1>
+      <h1>Puppy Bowl</h1>
+      <PlayerDetails player={player} />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Breed</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player) => {
+            return (
+              <tr key={player.id}>
+                <td>{player.name}</td>
+                <td>{player.breed}</td>
+                <td>{player.status}</td>
+                <td>
+                  <button onClick={() => handlePlayerClick(player.id)}>
+                    View Player
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+// import { useEffect, useState } from 'react'
+// import './App.css'
+// import { getPlayers, getPlayer } from "./api"
+
+// function App() {
+//   const [players, setPlayers] = useState([]);
+//   const [player, setPlayer] = useState({});
+
+//   useEffect(() => {
+//     getPlayers().then(setPlayers);
+//   });
+
+//   function handlePlayerClick(playerId) {
+//     getPlayer(playerId).then(setPlayer);
+//   }
+
+//   return (
+//     <>
+//       <h1>Puppy Bowl</h1>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Breed</th>
+//             <th>Status</th>
+//             <th>Action</th>
+//           </tr>
+//         </thead>
+//       </table>
+//       {players.map((player) => {
+//         return (
+//           <tr key={player.id}>
+//             <td>{player.name}</td>
+//             <td>{player.breed}</td>
+//             <td>{player.status}</td>
+//             <td>
+//               <button onClick={() => handlePlayerClick(player)}>View details</button>
+//             </td>
+//           </tr>
+//         )
+//       })}
+//     </>
+//   )
+// }
+
+// export default App
