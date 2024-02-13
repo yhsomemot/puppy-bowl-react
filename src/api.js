cf43bc5f2b8c7d9f93a3973dd503d07748d1c2e0
Young <youngheesomemoto@gmail.com>
2024-02-12 21:04:05 -0600

    smirk

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-const API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2308-ACC-ET-WEB-PT";
+const API_URL = "https://fsa-puppy-bowl.herokuapp.com/api/2308-ac-pt-web-pt-a";
 
 export async function getPlayers() {
     try {
-        const response = await fetch (`${API_URL}/players`);
+        const response = await fetch(`${API_URL}/players`);
         const result = await response.json();
         return result.data.players;
     } catch (error) {
@@ -12,7 +12,7 @@ export async function getPlayers() {
 
 export async function getPlayer(playerId) {
     try {
-        const response = await fetch (`${API_URL}/players/${playerId}`);
+        const response = await fetch(`${API_URL}/players/${playerId}`);
         const result = await response.json();
         return result.data.player;
     } catch (error) {
@@ -22,15 +22,36 @@ export async function getPlayer(playerId) {
 
 export async function createPlayer(player) {
     try {
-        const response = await fetch (`${API_URL}/players/${player}`, {
+        const response = await fetch(`${API_URL}/players`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: 
+            body: JSON.stringify(player),
         });
         const result = await response.json();
-        return result.data.Newplayer;
+        return result.data.newPlayer;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function deletePlayer(playerId) {
+    try {
+        const response = await fetch(`${API_URL}/players/${playerId}`, {
+            method: "DELETE",
+        });
+        await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getTeams() {
+    try {
+        const response = await fetch(`${API_URL}/teams`);
+        const result = await response.json();
+        return result.data.teams;
     } catch (error) {
         console.error(error);
     }
