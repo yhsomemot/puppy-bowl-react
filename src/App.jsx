d68ca96480489fd26de490cb2096cd311ffdf900
Young <youngheesomemoto@gmail.com>
2024-02-14 15:27:13 -0600

    did player componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { getPlayers, getPlayer } from "./api";
+import { Player } from "./components/Player";
 import { PlayerDetails } from "./components/PlayerDetails";
+
 
 function App() {
   const [players, setPlayers] = useState([]);
   const [player, setPlayer] = useState({});
 
   useEffect(() => {
+    // getPlayers().then(setPlayers);
     getPlayers().then((players) => {
       setPlayers(players);
     });
@@ -28,7 +31,7 @@ function App() {
   function handleSubmit(evt) {
     evt.preventDefault();
     const formData = newFormData(evt.target);
-    
+
   }
   return (
     <>
@@ -40,22 +43,15 @@ function App() {
             <th>Name</th>
             <th>Breed</th>
             <th>Status</th>
-            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {players.map((player) => {
             return (
-              <tr key={player.id}>
-                <td>{player.name}</td>
-                <td>{player.breed}</td>
-                <td>{player.status}</td>
-                <td>
-                  <button onClick={() => handlePlayerClick(player.id)}>
-                    View Player
-                  </button>
-                </td>
-              </tr>
+              <Player key={player.id} 
+              player={player} 
+              onClick={handlePlayerClick} 
+              />
             );
           })}
         </tbody>
@@ -65,52 +61,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import { useEffect, useState } from 'react'
-// import './App.css'
-// import { getPlayers, getPlayer } from "./api"
-
-// function App() {
-//   const [players, setPlayers] = useState([]);
-//   const [player, setPlayer] = useState({});
-
-//   useEffect(() => {
-//     getPlayers().then(setPlayers);
-//   });
-
-//   function handlePlayerClick(playerId) {
-//     getPlayer(playerId).then(setPlayer);
-//   }
-
-//   return (
-//     <>
-//       <h1>Puppy Bowl</h1>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Breed</th>
-//             <th>Status</th>
-//             <th>Action</th>
-//           </tr>
-//         </thead>
-//       </table>
-//       {players.map((player) => {
-//         return (
-//           <tr key={player.id}>
-//             <td>{player.name}</td>
-//             <td>{player.breed}</td>
-//             <td>{player.status}</td>
-//             <td>
-//               <button onClick={() => handlePlayerClick(player)}>View details</button>
-//             </td>
-//           </tr>
-//         )
-//       })}
-//     </>
-//   )
-// }
-
-// export default App
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,16 @@
 export function Player({ player, onClick }) {
-    
+
+    return (
+        <tr key={player.id}>
+            <td>{player.name}</td>
+            <td>{player.breed}</td>
+            <td>{player.status}</td>
+            <td>
+                <button onClick={() => onClick(player.id)}>
+                    View Player
+                </button>
+            </td>
+        </tr>
+    );
+
 }
