30d0aa2588d26cdf6b835cccb46c95f34ab4ba30
Young <youngheesomemoto@gmail.com>
2024-02-14 19:18:12 -0600

    form add player

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { getPlayers, getPlayer, deletePlayer } from "./api";
+import { getPlayers, getPlayer, deletePlayer, createPlayer } from "./api";
 import { Player } from "./components/Player";
 import { PlayerDetails } from "./components/PlayerDetails";
 
@@ -30,13 +30,28 @@ function App() {
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    const formData = newFormData(evt.target);
+    const formData = new FormData(evt.target);
+    const newPlayer = Object.fromEntries(formData.entries());
+    console.log(newPlayer)
+    createPlayer(newPlayer).then(() => {
+      getPlayers().then((players) => {
+        setPlayers(players);
+      });
+    })
 
   }
+
   return (
     <>
       <h1>Puppy Bowl</h1>
       <PlayerDetails player={player} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" />
+        <label htmlFor="breed">Breed:</label>
+        <input type="text" name="breed" />
+        <button type="submit">Add Player</button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -48,10 +63,10 @@ function App() {
         <tbody>
           {players.map((player) => {
             return (
-              <Player key={player.id} 
-              player={player} 
-              onClick={handlePlayerClick}
-              onDelete={handlePlayerDelete} 
+              <Player key={player.id}
+                player={player}
+                onClick={handlePlayerClick}
+                onDelete={handlePlayerDelete}
               />
             );
           })}
